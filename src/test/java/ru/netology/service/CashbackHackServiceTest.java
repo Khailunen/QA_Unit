89e1700706fe09429ea40faadc8601d278641a3b
Karina Khaltunen <khaltunen@bk.ru>
2024-12-01 18:47:23 +0300

    Add branch 'junit4 and change class 'CashbackHackServiseTest'

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,9 +1,10 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
-public class CashbackHackServiceTest{
+import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
     @Test
     public void remainAmount() {
         CashbackHackService service = new CashbackHackService();
@@ -11,7 +12,18 @@ public class CashbackHackServiceTest{
         int actual = service.remain(1500);
         int expected = 500;
 
-        Assert.assertEquals(actual, expected, "in order to get a bonus, you need to spend : " + expected);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void remainAmountZero() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(expected, actual);
 
     }
 }
