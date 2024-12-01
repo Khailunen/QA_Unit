9f323ad360c13f028e1dcab68fff1acb4dc96a2e
Karina Khaltunen <khaltunen@bk.ru>
2024-12-01 17:51:53 +0300

    Add branch 'testng' and new class 'CashbackHackServiceTest'

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,17 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class CashbackHackServiceTest{
+
+    @Test
+    public void remainAmount() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1500);
+        int expected = 500;
+
+        Assert.assertEquals(actual, expected, "in order to get a bonus, you need to spend : " + expected);
+
+    }
+}
